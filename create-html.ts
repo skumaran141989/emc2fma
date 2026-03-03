e03e5d5cfeb4c368b708a42e41deccfc13ec6394
Sakthi Kumaran <skumaran141989@gmail.com>
2026-03-03 13:33:10 +0530

    sitemap added

--- create-html.ts
+++ create-html.ts
@@ -2,13 +2,16 @@
 import fs from 'fs'
 import { parse } from 'node-html-parser';
 function createHTML(){
+     
       const allfiles = fs.readdirSync('./src/pages', { recursive: true, withFileTypes:true })
    
       const htmlContent =fs.readFileSync('./src/template.html', 'utf-8');
+      let urls = ``
       allfiles.filter((file)=>file.isFile()).forEach((fileType) => {
 
           const file = `${fileType.parentPath}/${fileType.name}`;
-            
+          urls=`${urls}${createSiteMapEntry(fileType.name.replace('.html',''))}`
+
           const htmlChildContent =fs.readFileSync(`${file}`, 'utf-8');
           const root = parse(htmlContent);
           const link = root.querySelector(`[href="./${fileType.name.replace('.html','')}"]`);
@@ -36,6 +39,20 @@ function createHTML(){
         }
          
       })
-
+      createSiteMap(urls)
 }
+
+function createSiteMapEntry(url:string){
+    const host=`https://wwww.emc2fma.com/`
+    return `<url><loc>${host}${url}</loc></url>\n`
+}
+
+function createSiteMap(urls:string){
+    const prelog =`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/0.9">\n`
+     
+      const postlog='</urlset>'
+
+      fs.writeFileSync(`./docs/sitemap.xml`, `${prelog}${urls}${postlog}`)
+}
+
 createHTML()
